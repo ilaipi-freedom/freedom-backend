c94454ccadb928958e5ed779c87cb258c39af006
Billy Yang <mz.yyam@gmail.com>
2023-07-10 21:34:07 +0800

    format as Asia Shanghai

--- src/common/date-helper.ts
+++ src/common/date-helper.ts
@@ -1,4 +1,6 @@
-import { format } from 'date-fns';
+import { format } from 'date-fns-tz';
 
 export const formatISO = (date: Date) =>
-  date ? format(date, "yyyy-MM-dd'T'HH:mm:ssxxx") : undefined;
+  date
+    ? format(date, "yyyy-MM-dd'T'HH:mm:ssxxx", { timeZone: 'Asia/Shanghai' })
+    : undefined;
